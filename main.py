b2bf6f4fe76217211fbf02c6cf3648974f543640
mdjcosta30 <mdjcosta30@gmail.com>
2024-09-01 20:06:40 +0200

    Feat: adding movement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tcod
 
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+
 def main():
     screen_width = 80
     screen_height = 50
@@ -11,6 +15,8 @@ def main():
     tileset = tcod.tileset.load_tilesheet(
         "./dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    event_handler = EventHandler()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -29,7 +35,16 @@ def main():
             context.present(root_console)
 
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 
