4088f0bf3c21b13410aeb5f9f94d14cbdc7fdb2c
mdjcosta30 <mdjcosta30@gmail.com>
2024-09-01 19:59:52 +0200

    feat: setting player x and y to center of screen

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     screen_width = 80
     screen_height = 50
 
+    player_x = screen_width // 2
+    player_y = screen_height // 2
+
     tileset = tcod.tileset.load_tilesheet(
         "./dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
@@ -21,7 +24,7 @@ def main():
         )
 
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
 
