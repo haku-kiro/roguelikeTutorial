965bdbe477e24e70e2de889072bf03533ce8a247
mdjcosta30 <mdjcosta30@gmail.com>
2024-09-01 20:07:32 +0200

    Fix: clearning console after movement

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ def main():
 
             context.present(root_console)
 
+            root_console.clear()
+
             for event in tcod.event.wait():
                 action = event_handler.dispatch(event)
 
